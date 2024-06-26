178593c7950e551c5034c32adf9734f1441efc95
bartucansahin <bartucan.dev@gmail.com>
2024-06-26 16:34:43 +0300

    Athlete Info section updates

--- data_sorter.py
+++ data_sorter.py
@@ -49,11 +49,41 @@ class Search_Bar:
         for match in matches:
             print(match)
 
+def display_info(value):
+    if value in df['Name'].values:
+        title = value
+        events_attended = set(df[df['Name'] == value]['Event'])
+        active_years = sorted(set(df[df['Name'] == value]['Year']))
+        years_list = list(active_years)
+
+        gold_medals = df[(df['Name'] == value) & (df['Medal'] == 'Gold')].shape[0]
+        silver_medals = df[(df['Name'] == value) & (df['Medal'] == 'Silver')].shape[0]
+        bronze_medals = df[(df['Name'] == value) & (df['Medal'] == 'Bronze')].shape[0]
+
+        total_medals = gold_medals + silver_medals + bronze_medals
+
+        attended_olympics = df[df['Name'] == value][['Year', 'City']].drop_duplicates()
+
+        print(title)
+        for event in events_attended:
+            print(event)
+        print(f"From {years_list[0]} to {years_list[-1]}")
+
+        if total_medals == 0:
+            print(f"{value} has not won any medals in The Olympics.")
+        else:
+            print(f"{gold_medals} Gold Medals")
+            print(f"{silver_medals} Silver Medals")
+            print(f"{bronze_medals} Bronze Medals")
+        
+        for index, row in attended_olympics.iterrows():
+            print(f"{row['Year']}, {row['City']}")
+
+        
 
 def main():
 
-    test = Search_Bar()
-    result = test.search("Ald")
+    display_info("Arvo Ossian Aaltonen")
 
 if __name__ == "__main__":
     main()
